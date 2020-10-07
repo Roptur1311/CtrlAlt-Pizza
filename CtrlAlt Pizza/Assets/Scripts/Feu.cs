a22935918aa5449f79e0bf64d600e4610f1214db
Samuel <s.miquel@rubika-edu.com>
2020-10-07 11:39:37 +0200

    Script Feu Working

---
+++ CtrlAlt Pizza/Assets/Scripts/Feu.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Feu : MonoBehaviour
+{
+    private bool feu;
+
+    void Start()
+    {
+        feu = true;
+    }
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+                feu = false;
+
+                Debug.Log(" Le feu est prêt à être allumé");
+        }
+
+        if (Input.GetKeyDown(KeyCode.D) && feu == false)
+        {
+                Debug.Log("Le feu est allumé");
+        }
+    }
+}
