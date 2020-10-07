766979dbc0de990e88e5b97392341f83ec11d499
Adel <adelou001@hotmail.com>
2020-10-07 17:10:21 +0200

    Oeuf big progress

--- CtrlAlt Pizza/Assets/Scripts/Oeuf.cs
+++ CtrlAlt Pizza/Assets/Scripts/Oeuf.cs
@@ -4,39 +4,106 @@ using UnityEngine;
 
 public class Oeuf : MonoBehaviour
 {
+    private bool rhythmIsDecided;
     private bool eggIsCracked;
     private bool firstButtonHit;
     private bool secondButtonHit;
     private int hitCount = 0;
-    private int hitTimeGap;
-
+    private float timer = 0.0f;
+    private float hitTimeGap = 0.0f;
 
     void Start()
-    {
+    {        
         
     }
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.P) && !firstButtonHit && !eggIsCracked)
-        {
+        if (Input.GetKeyDown(KeyCode.P) && !firstButtonHit && !secondButtonHit && !eggIsCracked && !rhythmIsDecided)
+        {          
             Debug.Log("First button hit");
-            firstButtonHit = true;
-            secondButtonHit = false;
+            firstButtonHit = true;            
+        }       
+
+        if (Input.GetKeyDown(KeyCode.Q) && firstButtonHit && !eggIsCracked && !rhythmIsDecided)
+        {
+            hitTimeGap = timer;
+            Debug.Log("Second button hit");
             hitCount += 1;
+            rhythmIsDecided = true;
         }
-        if (Input.GetKeyDown(KeyCode.Q) && !secondButtonHit && !eggIsCracked)
+
+        if (Input.GetKeyDown(KeyCode.Q) && !firstButtonHit && !secondButtonHit && !eggIsCracked && !rhythmIsDecided)
         {
             Debug.Log("Second button hit");
-            secondButtonHit = true;
-            firstButtonHit = false;
-            hitCount += 1;
+            secondButtonHit = true;           
         }
+
+        if (Input.GetKeyDown(KeyCode.P) && secondButtonHit && !eggIsCracked && !rhythmIsDecided)
+        {
+            hitTimeGap = timer;
+            Debug.Log("First button hit");
+            hitCount += 1;
+            rhythmIsDecided = true;
+        }
+
+        if (firstButtonHit || secondButtonHit)
+        {
+            timer += Time.deltaTime;
+        }
+
+        if (Input.GetKeyDown(KeyCode.T) && rhythmIsDecided)
+        {
+            Debug.Log(hitTimeGap);
+        }
+
+        /*if (!rhythmIsDecided)
+        {
+            StartCoroutine(Rhythm());
+        }*/
+
         if (hitCount == 10)
         {
             eggIsCracked = true;
             Debug.Log("Egg cracked");
             hitCount = 0;
+        }       
+    }
+
+    IEnumerator Rhythm()
+    {      
+        if (Input.GetKeyDown(KeyCode.P) && !firstButtonHit && !eggIsCracked)
+        {
+            timer += Time.deltaTime;
+            Debug.Log("First button hit");
+            firstButtonHit = true;
+            secondButtonHit = false;
+
+            if (Input.GetKeyDown(KeyCode.Q) && !secondButtonHit && !eggIsCracked)
+            {
+                hitTimeGap = timer;
+                Debug.Log("Second button hit");
+                secondButtonHit = true;
+                firstButtonHit = false;
+                rhythmIsDecided = true;
+            }
         }
+
+        if (Input.GetKeyDown(KeyCode.Q) && !secondButtonHit && !eggIsCracked)
+        {
+            timer += Time.deltaTime;
+            Debug.Log("Second button hit");
+            secondButtonHit = true;
+            firstButtonHit = false;
+            if (Input.GetKeyDown(KeyCode.P) && !secondButtonHit && !eggIsCracked)
+            {
+                hitTimeGap = timer;
+                Debug.Log("First button hit");
+                firstButtonHit = true;
+                secondButtonHit = false;
+                rhythmIsDecided = true;
+            }
+        }               
+        yield return null;
     }
 }
